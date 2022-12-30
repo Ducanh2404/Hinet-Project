6a10c1c97d8885b4da2f66dad439ccd15be16e44
Ducanh2404 <1951060533@e.tlu.edu.vn>
2022-12-31 05:04:56 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Mở đóng nút youtube
 let close_modal = document.getElementById('btn-esc-modal')
 let modal = document.getElementById('modal')
 let open_modal = document.getElementById('open-modal')
@@ -9,3 +10,8 @@ close_modal.addEventListener('click',function(){
     
     modal.style.visibility ="hidden";
 })
+
+//animation nhân vật
+const canvas = document.querySelector('canvas')
+const img    = canvas.toDataURL('image/png')
+document.getElementById('raze').src = img
