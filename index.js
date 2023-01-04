6043bd5e74eda2ec6035af7f1d366d42f59a4923
Ducanh2404 <1951060533@e.tlu.edu.vn>
2023-01-04 20:33:22 +0700

    update

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ let modal = document.getElementById('modal')
 let open_modal = document.getElementById('open-modal')
 
 open_modal.addEventListener('click',function(){
-    modal.style.visibility="visible";
+    modal.style.display="initial";
 })
 close_modal.addEventListener('click',function(){
     
-    modal.style.visibility ="hidden";
+    modal.style.display ="none";
 })
 
 //animation nhân vật
 
 //button chơi ngay
-let btn_pl= document.getElementById("btn-playnow")
+let open_pl= document.getElementById("btn-playnow")
+let modal_pl=document.getElementById('modal-plnow')
+let close_pl=document.getElementById('btn-esc-modal-plnow')
+let btn_free=document.getElementById('freeplay')
+open_pl.addEventListener('click',function(){
+    modal_pl.style.display="initial";
+})
+btn_free.addEventListener('click',function(){
+    modal_pl.style.display="initial";
+})
+close_pl.addEventListener('click',function(){
+    
+    modal_pl.style.display ="none";
+})
